7f6004dbf69537ac15ee9a6fe38053c84ba759a2
NikolayGromov <43181570+NikolayGromov@users.noreply.github.com>
2019-03-26 19:54:48 +0300

    Add files via upload

---
+++ pol/pol.cpp
@@ -0,0 +1,248 @@
+#include"pol.h"
+#include <iostream>
+using namespace std;
+double eps = 0.00000000001;
+	List::List()
+	{
+		n = 0;
+		head = NULL;
+		tail = head;
+	}
+	bool List::Isempty()
+	{
+		return(head == NULL);
+	}
+
+	void List::Insert_end(int val1, double val2)
+	{
+		n++;
+		Node* c = new Node();
+		c->p = val1;
+		c->c = val2;
+		c->next = NULL;
+		if (Isempty())
+			head = c;
+		else
+			tail->next = c;
+		tail = c;
+
+	}
+	void List::Insert_begin(int val1, double val2)
+	{
+		n++;
+		Node* tmp = new Node();
+		tmp->p = val1;
+		tmp->c = val2;
+		tmp->next = head;
+		if (head == NULL)
+			tail = tmp;
+		head = tmp;
+	}
+	void  List::Del(int k)
+	{
+		Node* f = head;
+		Node*b = head;
+		if (head == nullptr)
+			return;
+
+		if (head->p == k)
+		{
+			head = head->next;
+			n--;
+			return;
+		}
+		while (f->next != NULL)
+		{
+			if (f->next->p == k)
+			{
+				b = f->next;
+				f->next = f->next->next;
+				delete b;
+				n--;
+				break;
+			}
+			f = f->next;
+		}
+		b = head;
+		while ((b != NULL) && (b->next != NULL))
+			b = b->next;
+		tail = b;
+	}
+	void List::Print(int k)
+	{
+		if (k >= n) {
+			std::cout << "Large number" << std::endl;
+			return;
+		}
+		Node* c = head;
+		for (int i = 0; i < k; i++)
+			c = c->next;
+		std::cout << c->c << '(' << c->p << ')';
+	}
+	List List::operator=(List a)
+	{
+		head = a.head;
+		tail = a.tail;
+		n = a.n;
+		return(*this);
+	}
+
+
+	pol::pol()
+	{
+		a.n = 0;
+		a.head = NULL;
+		a.tail = a.head;
+	}
+	pol::pol(List b)
+	{
+		a = b;
+	}
+	pol pol::operator=(pol b)
+	{
+		a = b.a;
+		return(*this);
+	}
+	pol::pol(int pow, double c)
+	{
+		a.Insert_begin(pow, c);
+	}
+	pol& pol::Mull(double k)
+	{
+		pol b;
+		b = (*this);
+		Node* tmp = b.a.head;
+		while (tmp->next != NULL)
+		{
+			tmp->c *= k;
+			tmp = tmp->next;
+		}
+		tmp->c *= k;
+		b.DelDups();
+		b.DelZero();
+		return(b);
+	}
+	void pol::DelDups()
+	{
+		int k;
+		Node* tmp = a.head;
+		Node* tmp1;
+		while (tmp->next != NULL)
+		{
+			//cout << 'e' << endl;
+			tmp1 = tmp->next;
+			while (tmp1 != NULL)
+			{
+				k = 0;
+				if (tmp->p == tmp1->p)
+				{
+					tmp1->c += tmp->c;
+					k = tmp->p;
+					tmp = tmp->next;
+					//cout << k << endl;
+					a.Del(k);
+					break;
+				}
+				tmp1 = tmp1->next;
+			}
+			//cout << k << endl;
+			if (!k)
+				tmp = tmp->next;
+		}
+	}
+	void pol::DelZero()
+	{
+		int k = 0;
+		Node* tmp = a.head;
+		Node* tmp1 = tmp;
+		while (tmp1->next != NULL)
+		{
+			if (abs(tmp1->c) < eps)
+				k++;
+			tmp1 = tmp1->next;
+		}
+		if (abs(tmp1->c) < eps)
+			k++;
+		while ((a.head != NULL) && (abs(a.head->c) < eps))
+		{
+			a.head = a.head->next;
+			a.n--;
+			k--;
+		}
+		while (k != 0)
+		{
+
+			tmp = a.head;
+
+			while ((tmp != NULL) && (tmp->next != NULL))
+			{
+				if (abs(tmp->next->c) < eps)
+				{
+					a.n--;
+					k--;
+					tmp->next = tmp->next->next;
+					break;
+				}
+				tmp = tmp->next;
+			}
+		}
+		tmp = a.head;
+		while ((tmp != NULL) && (tmp->next != NULL))
+			tmp = tmp->next;
+		a.tail = tmp;
+	}
+	pol& pol::operator+(pol b)
+	{
+
+		Node* tmp1 = a.head;
+		Node* tmp2 = b.a.head;
+		a.tail->next = b.a.head;
+		a.tail = b.a.tail;
+
+		a.n += b.a.n;
+		DelDups();
+		DelZero();
+		return(*this);
+	}
+	pol& pol::operator*(pol b)
+	{
+		pol c;
+		Node* tmp1 = a.head;
+		Node* tmp2 = b.a.head;
+		while (tmp1->next != NULL)
+		{
+			tmp2 = b.a.head;
+			while (tmp2->next != NULL)
+			{
+				if ((((tmp1->p) / 100 + (tmp2->p) / 100) > 9) || ((((tmp1->p) % 100) / 10 + ((tmp2->p) % 100) / 10) > 9) || (((tmp1->p) % 10 + (tmp2->p) % 10) > 9))
+					throw "Incorrect power";
+				c.a.Insert_end(tmp1->p + tmp2->p, tmp1->c*tmp2->c);
+				tmp2 = tmp2->next;
+			}
+			if ((((tmp1->p) / 100 + (tmp2->p) / 100) > 9) || ((((tmp1->p) % 100) / 10 + ((tmp2->p) % 100) / 10) > 9) || (((tmp1->p) % 10 + (tmp2->p) % 10) > 9))
+				throw "Incorrect power";
+			c.a.Insert_end(tmp1->p + tmp2->p, tmp1->c*tmp2->c);
+			tmp1 = tmp1->next;
+		}
+		tmp2 = b.a.head;
+		while (tmp2->next != NULL)
+		{
+			if ((((tmp1->p) / 100 + (tmp2->p) / 100) > 9) || ((((tmp1->p) % 100) / 10 + ((tmp2->p) % 100) / 10) > 9) || (((tmp1->p) % 10 + (tmp2->p) % 10) > 9))
+				throw "Incorrect power";
+			c.a.Insert_end(tmp1->p + tmp2->p, tmp1->c*tmp2->c);
+			tmp2 = tmp2->next;
+		}
+		if ((((tmp1->p) / 100 + (tmp2->p) / 100) > 9) || ((((tmp1->p) % 100) / 10 + ((tmp2->p) % 100) / 10) > 9) || (((tmp1->p) % 10 + (tmp2->p) % 10) > 9))
+			throw "Incorrect power";
+		c.a.Insert_end(tmp1->p + tmp2->p, tmp1->c*tmp2->c);
+		c.DelDups();
+		c.DelZero();
+		return(c);
+	}
+	pol& pol::operator-(pol b)
+	{
+		pol c;
+		c = b;
+		c.Mull(-1);
+		return(*this + c);
+	}
--- pol/pol.h
+++ pol/pol.h
@@ -3,7 +3,6 @@
 #define UNTITLED_LIST_H
 #include <iostream>
 using namespace std;
-double eps = 0.00000000001;
 struct Node
 {
 	double c;
@@ -19,280 +18,28 @@ public:
 	Node* tail;
 
 	int n;
-	List()
-	{
-		n = 0;
-		head = NULL;
-		tail = head;
-	}
-	bool Isempty()
-	{
-		return(head == NULL);
-	}
-
-	void Insert_end(int val1, double val2)
-	{
-		n++;
-		Node* c = new Node();
-		c->p = val1;
-		c->c = val2;
-		c->next = NULL;
-		if (Isempty())
-			head = c;
-		else
-			tail->next = c;
-		tail = c;
-
-	}
-	void Insert_begin(int val1, double val2)
-	{
-		n++;
-		Node* tmp = new Node();
-		tmp->p = val1;
-		tmp->c = val2;
-		tmp->next = head;
-		if (head == NULL)
-			tail = tmp;
-		head = tmp;
-	}
-	void Del(int k)
-	{
-		Node* c = head;
-		if (head == nullptr)
-			return;
-
-		if (head->p == k)
-		{
-			head = head->next;
-			n--;
-			return;
-		}
-		while (c->next != NULL)
-		{
-			if (c->next->p == k)
-			{
-				c->next = c->next->next;
-				n--;
-				break;
-			}
-			c = c->next;
-		}
-		c = head;
-		while ((c != NULL) && (c->next != NULL))
-			c = c->next;
-		tail = c;
-
-	}
-	void Print(int k)
-	{
-		if (k >= n) {
-			std::cout << "Large number" << std::endl;
-			return;
-		}
-		Node* c = head;
-		for (int i = 0; i < k; i++)
-			c = c->next;
-		std::cout << c->c << '(' << c->p << ')';
-	}
-	List operator=(List a)
-	{
-		head = a.head;
-		tail = a.tail;
-		n = a.n;
-		return(*this);
-	}
-
+	List();
+	bool Isempty();
+	void Insert_end(int val1, double val2);
+	void Insert_begin(int val1, double val2);
+	void Del(int k);
+	void Print(int k);
+	List operator=(List a);
 };
 
 class pol
 {
 public:
 	List a;
-	pol()
-	{
-		a.n = 0;
-		a.head = NULL;
-		a.tail = a.head;
-	}
-	pol(List b)
-	{
-		a = b;
-	}
-	pol operator=(pol b)
-	{
-		a = b.a;
-		return(*this);
-	}
-	pol(int pow, double c)
-	{
-		a.Insert_begin(pow, c);
-	}
-	pol& Mull(double k)
-	{
-		pol b;
-		b = (*this);
-		Node* tmp = b.a.head;
-		while (tmp->next != NULL)
-		{
-			tmp->c *= k;
-			tmp = tmp->next;
-		}
-		tmp->c *= k;
-		b.DelDups();
-		b.DelZero();
-		return(b);
-	}
-	void DelDups()
-	{
-		Node* tmp = a.head;
-		Node* tmp1;
-		while (tmp->next != NULL)
-		{
-			tmp1 = tmp->next;
-			while (tmp1 != NULL)
-			{
-				if (tmp->p == tmp1->p)
-				{
-					tmp1->c += tmp->c;
-					a.Del(tmp->p);
-					break;
-				}
-				tmp1 = tmp1->next;
-			}
-			tmp = tmp->next;
-		}
-	}
-	void DelZero()
-	{
-		int k = 0;
-		Node* tmp = a.head;
-		Node* tmp1 = tmp;
-		while (tmp1->next != NULL)
-		{
-			if (abs(tmp1->c) < eps)
-				k++;
-			tmp1 = tmp1->next;
-		}
-		if (abs(tmp1->c) < eps)
-			k++;
-		while ((a.head != NULL) && (abs(a.head->c) < eps))
-		{
-			a.head = a.head->next;
-			a.n--;
-			k--;
-		}
-		while (k != 0)
-		{
-
-			tmp = a.head;
-
-			while ((tmp != NULL) && (tmp->next != NULL))
-			{
-				if (abs(tmp->next->c) < eps)
-				{
-					a.n--;
-					k--;
-					tmp->next = tmp->next->next;
-					break;
-				}
-				tmp = tmp->next;
-			}
-		}
-		tmp = a.head;
-		while ((tmp != NULL) && (tmp->next != NULL))
-			tmp = tmp->next;
-		a.tail = tmp;
-	}
-	pol& operator+(pol b)
-	{
-		Node* tmp1 = a.head;
-		Node* tmp2 = b.a.head;
-		while (tmp1->next != NULL)
-		{
-			tmp2 = b.a.head;
-			while (tmp2->next != NULL)
-			{
-				if (tmp1->p == tmp2->p)
-				{
-					tmp1->c += tmp2->c;
-					b.a.Del(tmp2->p);
-				}
-				tmp2 = tmp2->next;
-			}
-			if (tmp1->p == tmp2->p)
-			{
-				tmp1->c += tmp2->c;
-				b.a.Del(tmp2->p);
-			}
-			tmp1 = tmp1->next;
-		}
-		tmp2 = b.a.head;
-		while ((tmp2 != NULL) && (tmp2->next != NULL))
-		{
-			if (tmp1->p == tmp2->p)
-			{
-				tmp1->c += tmp2->c;
-				b.a.Del(tmp2->p);
-			}
-			tmp2 = tmp2->next;
-		}
-		if (tmp2 != NULL)
-			if (tmp1->p == tmp2->p)
-			{
-				tmp1->c += tmp2->c;
-				b.a.Del(tmp2->p);
-			}
-		a.tail->next = b.a.head;
-		if (b.a.head != NULL)
-			a.tail = b.a.tail;
-
-		a.n += b.a.n;
-		DelDups();
-		DelZero();
-		return(*this);
-	}
-	pol& operator*(pol b)
-	{
-		pol c;
-		Node* tmp1 = a.head;
-		Node* tmp2 = b.a.head;
-		while (tmp1->next != NULL)
-		{
-			tmp2 = b.a.head;
-			while (tmp2->next != NULL)
-			{
-				if ((((tmp1->p) / 100 + (tmp2->p) / 100) > 9) || ((((tmp1->p) % 100) / 10 + ((tmp2->p) % 100) / 10) > 9) || (((tmp1->p) % 10 + (tmp2->p) % 10) > 9))
-					throw "Incorrect power";
-				c.a.Insert_end(tmp1->p + tmp2->p, tmp1->c*tmp2->c);
-				tmp2 = tmp2->next;
-			}
-			if ((((tmp1->p) / 100 + (tmp2->p) / 100) > 9) || ((((tmp1->p) % 100) / 10 + ((tmp2->p) % 100) / 10) > 9) || (((tmp1->p) % 10 + (tmp2->p) % 10) > 9))
-				throw "Incorrect power";
-			c.a.Insert_end(tmp1->p + tmp2->p, tmp1->c*tmp2->c);
-			tmp1 = tmp1->next;
-		}
-		tmp2 = b.a.head;
-		while (tmp2->next != NULL)
-		{
-			if ((((tmp1->p) / 100 + (tmp2->p) / 100) > 9) || ((((tmp1->p) % 100) / 10 + ((tmp2->p) % 100) / 10) > 9) || (((tmp1->p) % 10 + (tmp2->p) % 10) > 9))
-				throw "Incorrect power";
-			c.a.Insert_end(tmp1->p + tmp2->p, tmp1->c*tmp2->c);
-			tmp2 = tmp2->next;
-		}
-		if ((((tmp1->p) / 100 + (tmp2->p) / 100) > 9) || ((((tmp1->p) % 100) / 10 + ((tmp2->p) % 100) / 10) > 9) || (((tmp1->p) % 10 + (tmp2->p) % 10) > 9))
-			throw "Incorrect power";
-		c.a.Insert_end(tmp1->p + tmp2->p, tmp1->c*tmp2->c);
-		c.DelDups();
-		c.DelZero();
-		return(c);
-	}
-	pol& operator-(pol b)
-	{
-		pol c;
-		c = b;
-		c.Mull(-1);
-		return(*this + c);
-	}
-
+	pol();
+	pol(List b);
+	pol operator=(pol b);
+	pol(int pow, double c);
+	pol& Mull(double k);
+	void DelDups();
+	void DelZero();
+	pol& operator+(pol b);
+	pol& operator*(pol b);
+	pol& operator-(pol b);
 };
 #endif UNTITLED_LIST_H
