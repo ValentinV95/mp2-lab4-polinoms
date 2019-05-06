3dd18753cd02a9f926cbd110c4a622314564d0ba
Bezaev Roman <31104632+trixdade@users.noreply.github.com>
2019-05-06 22:04:26 +0300

    fix bad merge

--- Polynom/polynoms/polynoms.cpp
+++ Polynom/polynoms/polynoms.cpp
@@ -98,13 +98,8 @@ void Polynom::merge(const Polynom &pol)
 	}
 	*this = tmp;
 
-<<<<<<< HEAD
-	tmp.head = NULL;
-	tmp.size = 0;
-=======
 	tmp.head = NULL;  
 	tmp.size = 0;  
->>>>>>> d8a5eb062f22558cf959c054a93ca5998c4f52f0
 }
 
 inline void Polynom::pop_front()
