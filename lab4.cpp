ccdb5f8dc66f7d40cdde7d8b4e9b6ff64c298989
gulash99 <daria.i.gulyaeva@gmail.com>
2019-06-04 22:53:05 +0300

    Lab4

--- lab4.cpp
+++ lab4.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 using namespace std;
+//#include <gtest/gtest.h>
 struct Monom
 {
 	int st;
-	int con;
-	Monom(int _st = 0, int _con = 0) : st(_st), con(_con) {};
+	double con;
+	Monom(int _st = 0, double _con = 0) : st(_st), con(_con) {};
 
 
 	int getX() const
 	{
-		return st/100;
+		return st / 100;
 	}
 	int getY() const
 	{
-		return (st/10)%10;
+		return (st / 10) % 10;
 	}
 	int getZ() const
 	{
-		return st%10;
+		return st % 10;
 	}
 	bool operator>(const Monom& second) const
 	{
@@ -43,23 +44,23 @@ struct Monom
 	Monom operator*(const Monom& second) const
 	{
 		Monom res;
-		int x,y,z;
-		z = this->getZ()+ second.getZ();
-		if(z > 9)
+		int x, y, z;
+		z = this->getZ() + second.getZ();
+		if (z > 9)
 		{
 			throw std::string("Z  > 9");
 		}
-		y = (this->getY()+ second.getY());
-		if(y > 9)
+		y = (this->getY() + second.getY());
+		if (y > 9)
 		{
 			throw std::string("Y  > 9");
 		}
-		x =(this->getX()+ second.getX());
-		if(x > 9)
+		x = (this->getX() + second.getX());
+		if (x > 9)
 		{
 			throw std::string("X  > 9");
 		}
-		res.st = x*100+y*10+z;
+		res.st = x * 100 + y * 10 + z;
 		res.con = this->con*second.con;
 		return res;
 	}
@@ -73,61 +74,62 @@ class Polynom
 	{
 		Monom value;
 		Node* next;
-		Node(Monom _val = Monom(0,0), Node* _next = NULL) : value(_val), next(_next) {};
+		Node(Monom _val = Monom(0, 0), Node* _next = NULL) : value(_val), next(_next) {};
 	};
 	int size = 0;
-	Node m_Head;
+	Node* m_Head = NULL;
 
-	Node* &m_Begin = m_Head.next;
+	Node* &m_Begin = m_Head;
 
 public:
-	Polynom(){};
+	Polynom() {};
 	void push(Monom val)
 	{
 		Node** tmp = &m_Begin;
 		Node* next = NULL;
-		for( ; (*tmp)!=NULL;tmp=&((*tmp)->next))
+		for (; (*tmp) != NULL; tmp = &((*tmp)->next))
 		{
-			if((*tmp)->value >= val)
-				{
-					Monom swap = (*tmp)->value;
-					next = (*tmp)->next;
-					(*tmp)->value = val;
-					val = swap;
-					tmp = &((*tmp)->next);
-					break;
-				}
+			if ((*tmp)->value >= val)
+			{
+				Monom swap = (*tmp)->value;
+				next = (*tmp)->next;
+				(*tmp)->value = val;
+				val = swap;
+				tmp = &((*tmp)->next);
+				break;
+			}
 		}
 		++size;
-		*tmp = new Node(val,next);
+		*tmp = new Node(val, next);
 	}
 	void clear()
 	{
 		Node* deleteLater;
-		for(Node* tmp = m_Begin; tmp!=NULL; delete deleteLater)
+		for (Node* tmp = m_Begin; tmp != NULL; delete deleteLater)
 		{
 			deleteLater = tmp;
-			tmp=tmp->next;
+			tmp = tmp->next;
 		}
+		size = 0;
+		m_Head = NULL;
 	}
 	void norm()
 	{
-
-		for(Node* tmp = m_Begin; tmp!=NULL; )
+		for (Node** tmp = &m_Begin; tmp != NULL; )
 		{
-			if(tmp->next)
+			if ((*tmp)->next)
 			{
-				if(tmp->value == tmp->next->value)
+				if ((*tmp)->value == (*tmp)->next->value)
 				{
-					tmp->value.con += tmp->next->value.con;
-					Node* deleteLayter = tmp->next;
-					tmp->next = deleteLayter->next;
+					(*tmp)->value.con += (*tmp)->next->value.con;
+					Node* deleteLayter = (*tmp)->next;
+					(*tmp)->next = deleteLayter->next;
 					--size;
 					delete deleteLayter;
 				}
 				else
 				{
-					tmp=tmp->next;
+					tmp = &(*tmp)->next;
 				}
 			}
 			else
@@ -135,44 +137,121 @@ public:
 				break;
 			}
 		}
-		for(Node** tmp = &m_Begin; (*tmp)!=NULL;tmp = &((*tmp)->next) )
+		for (Node** tmp = &m_Begin; (*tmp) != NULL; tmp = &((*tmp)->next))
 		{
-			if((*tmp)->value.con == 0)
+			if ((*tmp)->value.con == 0)
 			{
 				Node* deleteLayter = *tmp;
-				(*tmp) = (*tmp)->next;
+				tmp = &(*tmp)->next;
 				delete deleteLayter;
 			}
-		}
+		};
 		out();
 	}
 	void out()
 	{
-		cout <<"MONOM"<< endl;
-		for(Node* tmp = m_Begin; tmp!=NULL;tmp = tmp->next )
+		cout << "MONOM" << endl;
+		for (Node* tmp = m_Begin; tmp != NULL; tmp = tmp->next)
 		{
-			cout << tmp->value.con << "*x^" <<tmp->value.getX() <<"*y^" << tmp->value.getY() <<"*z^" << tmp->value.getZ() <<endl;
+			cout << tmp->value.con << "*x^" << tmp->value.getX() << "*y^" << tmp->value.getY() << "*z^" << tmp->value.getZ() << endl;
 		}
+		cout << "end MONOM" << endl;
 	}
-	void mult(int multi)
+	Polynom operator*(double multi) const
 	{
-		for(Node* tmp = m_Begin; tmp!=NULL; tmp=tmp->next)
+		Polynom polynom;
+		Monom monom;
+		for (Node* tmp = m_Begin; tmp != NULL; tmp = tmp->next)
 		{
-			tmp->value.con*=multi;
+			monom = tmp->value;
+			monom.con *= multi;
+			polynom.push(monom);
 		}
-		norm();
+		polynom.norm();
+		return polynom;
+	}
+
+	Polynom operator*(const Polynom& second) const
+	{
+		Polynom res;
+		for (unsigned i = 0; i < this->Size(); ++i)
+		{
+			for (unsigned j = 0; j < second.Size(); ++j)
+			{
+				try
+				{
+					res.push(this->at(i) * second.at(j));
+				}
+				catch (std::string &st)
+				{
+					cout << st << endl << "Exit: -1" << endl;
+					exit(-1);
+				}
+			}
+		}
+
+		res.norm();
+
+		return res;
+	}
+	Polynom operator+(const Polynom& second) const
+	{
+		Polynom result;
+		int i = 0, j = 0, n = this->Size(), m = second.Size();
+		int size = n + m;
+
+		while (i < n && j < m)
+		{
+			if (this->at(i) >= second.at(j))
+			{
+				result.push(second.at(j));
+				++j;
+			}
+			else
+			{
+				result.push(this->at(i));
+				++i;
+			}
+		}
+		while (i < n)
+		{
+			result.push(this->at(i));
+			++i;
+		}
+		while (j < n)
+		{
+			result.push(second.at(j));
+			++j;
+		}
+		result.norm();
+		return result;
+	}
+	Polynom operator-(const Polynom& second) const
+	{
+
+		return (*this) + second * -1;
+	}
+	Polynom& operator=(const Polynom& second)
+	{
+
+		this->clear();
+		for (Node* tmp = second.m_Begin; tmp != NULL; tmp = tmp->next)
+		{
+			this->push(tmp->value);
+		}
+		return *this;
 	}
 	bool operator==(const Polynom& second) const
 	{
-		if(this->size != second.size)
+		if (this->size != second.size)
 		{
 			return false;
 		}
 		Node* tmp1 = this->m_Begin;
 		Node* tmp2 = second.m_Begin;
-		while(tmp1 != NULL)
+		while (tmp1 != NULL)
 		{
-			if(tmp1->value.st != tmp2->value.st && tmp1->value.con != tmp2->value.con)
+			if (tmp1->value.st != tmp2->value.st && tmp1->value.con != tmp2->value.con)
 			{
 				return false;
 			}
@@ -181,12 +260,12 @@ public:
 		}
 		return true;
 	}
-	 Monom at(int index) const
+	Monom at(int index) const
 	{
 		Node* tmp = m_Begin;
-		for(int i = 0; i !=index; ++i)
+		for (int i = 0; i != index; ++i)
 		{
-			tmp=tmp->next;
+			tmp = tmp->next;
 		}
 		return tmp->value;
 	}
@@ -198,6 +277,7 @@ public:
 	{
 		clear();
 	}
+
 };
 Polynom* createPolynom()
 {
@@ -207,7 +287,7 @@ Polynom* createPolynom()
 	cin >> n;
 	Polynom* pol = new Polynom;
 	Monom element;
-	for(int i = 0; i < n; ++i)
+	for (int i = 0; i < n; ++i)
 	{
 		cout << "Vvedite stepen'" << endl;
 		cin >> element.st;
@@ -217,121 +297,96 @@ Polynom* createPolynom()
 	}
 	return pol;
 }
-Polynom* sum(Polynom* first, Polynom* second)
+
+/*TEST(testPolynom, PloynomMultConstTest)
 {
-	Polynom* result = new Polynom();
-	int i = 0, j = 0, n = first->Size(), m = second->Size();
-	int size = n + m;
+	Polynom pol1;
+	pol1.push(Monom(123, 1));
+	pol1.push(Monom(234, 2));
+	Polynom pol2;
 
-	while(i < n && j < m)
-		{
-			if(first->at(i) >= second->at(j))
-			{
-				result->push(second->at(j));
-				++j;
-			}
-			else
-			{
-				result->push(first->at(i));
-				++i;
-			}
-		}
-		while(i < n)
-		{
-			result->push(first->at(i));
-			++i;
-		}
-		while(j < n)
-		{
-			result->push(second->at(j));
-			++j;
-		}
-/*	delete first;
-	delete second;*/
-	result->norm();
+	pol2.push(Monom(123, 2));
 
-	return result;
+	pol2.push(Monom(234, 4));
+
+	pol1 = pol1 * 2;
+	pol2.out();
+
+	EXPECT_EQ(pol2, pol1);
+
 }
-
-Polynom* mins(Polynom* first, Polynom* second)
+TEST(testPolynom, PloynomSumTest)
 {
-	second->mult(-1);
-	return sum(first,second);
-}
-Polynom* mult(Polynom* first, Polynom* second)
-{
-	Polynom* res = new Polynom();
-	for(unsigned i = 0; i < first->Size(); ++i)
-	{
-		for(unsigned j = 0; j < second->Size(); ++j)
-		{
-			try
-			{
-				res->push(first->at(i) * second->at(j));
-			}catch(std::string &st)
-			{
-				cout << st << endl << "Exit: -1" <<endl;
-				exit(-1);
-			}
-		}
-	}
-	res->norm();
-	return res;
-}
+	Polynom pol1;
+	pol1.push(Monom(123, 1));
+	pol1.push(Monom(234, 2));
+	Polynom pol2;
+	pol2.push(Monom(123, 2));
+	pol2.push(Monom(234, 4));
+	Polynom res = pol2 + pol1;
+	Polynom answer;
+	answer.push(Monom(123, 3));
+	answer.push(Monom(234, 6));
 
+	EXPECT_EQ(answer, res);
+}*/
 int main(int argc, char *argv[])
 {
-
+	/*::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();*/
 	Polynom* pol1 = createPolynom();
 	pol1->out();
 	bool quit = false;
 	do
 	{
-		cout << "vibirite deistvie" << endl;
-		cout << "1-SUM" << endl;
-		cout << "2-MINUS" << endl;
-		cout << "3-MULT" << endl;
-		cout << "4-MULT CONST" << endl;
-		cout << "5-Exit" << endl;
-		int action;
+	cout << "vibirite deistvie" << endl;
+	cout << "1-SUM" << endl;
+	cout << "2-MINUS" << endl;
+	cout << "3-MULT" << endl;
+	cout << "4-MULT CONST" << endl;
+	cout << "5-Exit" << endl;
+	int action;
 
-		cin >> action;
-		quit = true;
+	cin >> action;
+	quit = true;
 
-		switch(action)
-		{
-			case 1:
-			{
-				Polynom* pol2 = createPolynom();
-				pol1 = sum(pol1,pol2);
-				break;
-			}
-			case 2:
-			{
-				Polynom* pol2 = createPolynom();
-				pol1 = mins(pol1,pol2);
-				break;
-			}
-			case 3:
-			{
-				Polynom* pol2 = createPolynom();
-				pol1 = mult(pol1,pol2);
-				break;
-			}
-			case 4:
-			{
-				cout << "Vvedite konstanty" << endl;
-				int n;
-				cin >> n;
-				pol1->mult(n);
-				break;
-			}
-			case 5:
-			{
-				quit = false;
-				break;
-			}
-		}
+	switch(action)
+	{
+	case 1:
+	{
+	Polynom* pol2 = createPolynom();
+	pol1 = sum(pol1,pol2);
+	break;
+	}
+	case 2:
+	{
+	Polynom* pol2 = createPolynom();
+	pol1 = mins(pol1,pol2);
+	break;
+	}
+	case 3:
+	{
+	Polynom* pol2 = createPolynom();
+	pol1 = mult(pol1,pol2);
+	break;
+	}
+	case 4:
+	{
+	cout << "Vvedite konstanty" << endl;
+	int n;
+	cin >> n;
+	pol1->mult(n);
+	break;
+	}
+	case 5:
+	{
+	quit = false;
+	break;
+	}
+	}
 	}while(quit);
+
 	delete pol1;
+
+
 }
