853c1e1d63fc857bede831ce1962f06d55f371f8
Vlada-K <62649143+Vlada-K@users.noreply.github.com>
2021-04-20 20:44:15 +0300

    Add files via upload

---
+++ Polinom.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "..\..\include\Monom.h"
+class Polinom
+{
+private:
+	Monom* head;
+public:
+	Polinom();
+	Polinom( const Polinom& p);
+	~Polinom();
+	void Add(Monom* m);
+	void Delete();
+	void Privide();
+	void Remove(Monom*j);
+	Polinom& operator=(Polinom p);
+	friend istream& operator>>(istream& in, Polinom& p);
+	friend ostream& operator<<(ostream& out, const Polinom& p);
+	string toString();
+	Polinom operator- (Polinom& p);
+	Polinom operator+ (Polinom& p);
+	Polinom operator* (Polinom& p);
+	Polinom operator*(double w) ;
+};
+
