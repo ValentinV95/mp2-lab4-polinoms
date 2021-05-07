e4e198a1932a34c004127e6d6c6e3a517a3c7eee
MascartKL <Mr.Mascart.kl@yandex.ru>
2021-05-07 23:29:15 +0300

    Lab 4

---
+++ Lab4/Polinoms/List.h
@@ -0,0 +1,136 @@
+#pragma once
+#include <cstdio>
+#include <stack>
+using namespace std;
+
+template <class ValType>
+struct Node
+{
+	ValType data;
+	Node <ValType> * next;
+	Node <ValType> * prev;
+	Node() : data()
+	{
+		next = nullptr;
+		prev = nullptr;
+	}
+};
+
+template <class ValType>
+class List
+{
+private:
+	Node <ValType> *head;
+	int size;
+	friend class Polinom;
+public:
+	List() : head(nullptr), size(0) {}
+
+	~List()
+	{
+		Node <ValType> * ptr = head;
+		while (ptr != nullptr)
+		{
+			head = ptr->next;
+			delete ptr;
+			ptr = head;
+		}
+	}
+	Node<ValType>* Root()
+	{
+		return head;
+	}
+	int Size()
+	{
+		return size;
+	}
+	void Insert(ValType M)
+	{
+		Node <ValType> *A = new Node<ValType>;
+		A->data = M;
+
+		if (size == 0)
+		{
+			head = A;
+		}
+		else
+		{
+			Node<ValType>* pos = nullptr;
+			Node<ValType>* ptr = head;
+			if (M > head->data)
+			{
+				head->prev = A;
+				A->next = head;
+				head = A;
+			}
+			else
+			{
+				while ((ptr->data >= M) && (ptr->next != nullptr))
+				{
+					ptr = ptr->next;
+				}
+				if (ptr->next != nullptr)
+				{
+					pos = ptr->prev;
+					A->next = pos->next;
+					pos->next->prev = A;
+					pos->next = A;
+					A->prev = pos;
+				}
+				else
+				{
+					ptr->next = A;
+					A->prev = ptr;
+				}
+			}
+
+		}
+		size++;
+	}
+	void Delete(ValType M)
+	{
+		int k = 0;
+		Node<ValType>* temp;
+		Node<ValType>* _temp;
+		temp = head;
+		while (k < size)
+		{
+			if (k == 0)
+			{
+				if (temp->data == M)
+				{
+					temp->next->prev = nullptr;
+					temp = temp->next;
+					delete head;
+					head = temp;
+					size--;
+					return;
+				}
+			}
+			else
+			{
+				if (temp->data == M)
+				{
+					_temp = temp;
+					temp->next->prev = temp->prev;
+					temp->prev->next = temp->next;
+					delete _temp;
+					size--;
+					return;
+				}
+			}
+			temp = temp->next;
+			k++;
+		}
+	}
+	void MergerNode(Node<ValType>* p1, Node<ValType>* p2)
+	{
+		p1->data = p1->data + p2->data;
+		p1->next = p2->next;
+		if (p2->next != nullptr)
+			p2->next->prev = p1;
+
+		delete p2;
+		size--;
+	}
+};
---
+++ Lab4/Polinoms/main_polinom.cpp
@@ -0,0 +1,35 @@
+﻿#include"Polinom.h"
+
+void main()
+{
+	Polinom A, B;
+	Polinom C, D, E;
+	int w = 1;
+	cout << "Demonstration of performing arithmetic operations of addition, subtraction and multiplication for polynomials." << endl;
+	while (w)
+	{
+		cout << endl << "Entering the polynomial A:  ";
+		A.Input(); cout << "A="; A.Print();  cout << endl << endl;
+		cout << "Entering the polynomial B:  ";
+		B.Input(); cout << "B="; B.Print();  cout << endl << endl;
+		C = A + B;
+		D = A - B;
+		E = A * B;
+		cout << "Addition:  "; cout << "A+B="; C.Print();  cout << endl;
+		cout << "Subtraction:  "; cout << "A-B="; D.Print();  cout << endl;
+		cout << "Multiplication:  ";
+
+		if (E.Highest_Degree() < 999)
+		{
+			cout << "A*B="; E.Print();  cout << endl << endl;
+		}
+		else
+			cout << "The degree of a polynomial is outside the scope of this implementation." << endl;
+
+		cout << "If you want to continue the program, press 1," << endl;
+		cout << "if you want to exit the program, press 0:";
+		cin >> w;
+	}
+
+	system("pause");
+}
