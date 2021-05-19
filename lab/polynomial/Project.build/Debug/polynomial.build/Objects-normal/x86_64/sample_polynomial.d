794bc9d2d70b458fc1aeac77cb7bb31789a7a805
VikaTomachinskaya <63149007+VikaTomachinskaya@users.noreply.github.com>
2021-05-19 22:25:44 +0300

    Delete sample_polynomial.d

--- lab/polynomial/Project.build/Debug/polynomial.build/Objects-normal/x86_64/sample_polynomial.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/viktoriatomacinskaya/Desktop/mp2-lab4-polinoms/polynomial/sample_polynomial.cpp \
-  /Users/viktoriatomacinskaya/Desktop/mp2-lab4-polinoms/polynomial/polynomial.h
