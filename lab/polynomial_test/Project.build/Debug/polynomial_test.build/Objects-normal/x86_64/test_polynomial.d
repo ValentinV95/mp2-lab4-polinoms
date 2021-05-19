77a32f9beba199a7ca7b38f59ad80074c02a003d
VikaTomachinskaya <63149007+VikaTomachinskaya@users.noreply.github.com>
2021-05-19 22:23:37 +0300

    Delete test_polynomial.d

--- lab/polynomial_test/Project.build/Debug/polynomial_test.build/Objects-normal/x86_64/test_polynomial.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/viktoriatomacinskaya/Desktop/mp2-lab4-polinoms/polynomial_test/test_polynomial.cpp \
-  /Users/viktoriatomacinskaya/Desktop/mp2-lab4-polinoms/polynomial/polynomial.h \
-  /Users/viktoriatomacinskaya/Desktop/mp2-lab4-polinoms/gtest/gtest.h
